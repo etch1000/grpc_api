d24942381dd4cbe7dbfc5ea462ee6b4e036ba577
etch1000 <hmoroliya@ariqt.com>
2023-01-16 05:32:28 +0530

    Basic gRPC server and a client

---
+++ src/server.rs
@@ -0,0 +1,37 @@
+use tonic::{transport::Server, Request, Response, Status};
+
+use hello::greeter_server::{Greeter, GreeterServer};
+use hello::{HelloReply, HelloRequest};
+
+pub mod hello {
+    tonic::include_proto!("hello");
+}
+
+#[derive(Default)]
+pub struct GreeterP {}
+
+#[tonic::async_trait]
+impl Greeter for GreeterP {
+    async fn say_hello(&self, request: Request<HelloRequest>) -> Result<Response<HelloReply>, Status> {
+        println!("Got a request from : {:#?}", request.remote_addr());
+
+        let reply = HelloReply {
+            message: format!("Hello {}!", request.into_inner().name).into(),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let address = "[::1]:50051".parse().unwrap();
+
+    let greeter = GreeterP::default();
+
+    println!("GreeterServer Listening on : {address:#?}");
+
+    Server::builder().add_service(GreeterServer::new(greeter)).serve(address).await?;
+
+    Ok(())
+} 
