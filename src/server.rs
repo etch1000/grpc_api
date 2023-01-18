a9367926907f510726133c567db892800b532684
etch1000 <hmoroliya@ariqt.com>
2023-01-18 10:54:14 +0530

    Cargo fmt

--- src/server.rs
+++ src/server.rs
@@ -12,7 +12,10 @@ pub struct GreeterP {}
 
 #[tonic::async_trait]
 impl Greeter for GreeterP {
-    async fn say_hello(&self, request: Request<HelloRequest>) -> Result<Response<HelloReply>, Status> {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
         println!("Got a request from : {:#?}", request.remote_addr());
 
         let reply = HelloReply {
@@ -31,7 +34,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("GreeterServer Listening on : {address:#?}");
 
-    Server::builder().add_service(GreeterServer::new(greeter)).serve(address).await?;
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(address)
+        .await?;
 
     Ok(())
-} 
+}
